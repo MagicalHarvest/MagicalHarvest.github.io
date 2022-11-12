327e712633a90d738149e8a356ee6011bc486b4c
IsaVII <isaSturm7@gmail.com>
2022-11-12 14:18:39 +0100

    Update pictures

---
+++ js/screenshots.js
@@ -0,0 +1,39 @@
+document.write(`
+<div class="DecoLine"></div>
+<div class="ScreenshotContainer">
+  <a href="images/featureImages/Magic.png" target="_blank">
+  <img src="images/featureImages/Magic.png" class="screenshots"></img>  </a>
+
+  <a href="images/featureImages/EvolvePlants.png" target="_blank">
+  <img src="images/featureImages/EvolvePlants.png" class="screenshots"></img>  </a>
+
+  <a href="images/featureImages/Animals.png" target="_blank">
+  <img src="images/featureImages/Animals.png" class="screenshots"></img>  </a>
+
+  <a href="images/featureImages/Potions.png" target="_blank">
+  <img src="images/featureImages/Potions.png" class="screenshots"></img>  </a>
+
+  <a href="images/featureImages/fishing.png" target="_blank">
+  <img src="images/featureImages/fishing.png" class="screenshots"></img>  </a>
+
+  <a href="images/featureImages/mine.png" target="_blank">
+  <img src="images/featureImages/mine.png" class="screenshots"></img>  </a>
+
+  <a href="images/screenshots/Forest.png" target="_blank">
+  <img src="images/screenshots/Forest.png" class="screenshots"></img>  </a>
+
+  <a href="images/otherImages/2022-07-30_AllDemoCrops.png" target="_blank">
+  <img src="images/otherImages/2022-07-30_AllDemoCrops.png" class="screenshots"></img>  </a>
+
+  <a href="images/screenshots/Forest.png" target="_blank">
+  <img src="images/screenshots/Forest.png" class="screenshots"></img>  </a>
+
+  <a href="images/blog/2022-07-14-fish.png" target="_blank">
+  <img src="images/blog/2022-07-14-fish.png" class="screenshots"></img>  </a>
+
+  <a href="images/blog/2022-07-14-illustration.png" target="_blank">
+  <img src="images/blog/2022-07-14-illustration.png" class="screenshots"></img>  </a>
+</div>
+
+<div class="DecoLine"></div>
+`);
