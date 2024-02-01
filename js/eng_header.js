9550cb3b96ffeae2ebd9b8e7c0d3c7e20a4457c5
IsaVII <isaSturm7@gmail.com>
2024-02-01 15:29:29 +0100

    Feb news 2024

--- js/eng_header.js
+++ js/eng_header.js
@@ -10,7 +10,7 @@ document.write(`
           <div class="link"><a href="world.html">World</a></div>
           <div class="link"><a href="cast_bachelors.html">Cast</a></div>
           <!--  <div class="link"><a href="farm.html">Farm</a></div> -->
-          <div class="link"><a href="blog_23.html">Blog</a></div>
+          <div class="link"><a href="blog_24.html">Blog</a></div>
           <div class="link"><a href="jobs_eng.html">Jobs</a></div>
         </ul>
 
@@ -20,6 +20,7 @@ document.write(`
       <u><a href="press/product/index.html" target="_blank">Presskit</a></u>
           <a href="https://discord.gg/8tXMkrMDyy" target="_blank"> <img src="images/DiscordIcon.png" alt="discord-icon"></img></a>
           <a href="https://twitter.com/MagicalHarvest" target="_blank"><img src="images/TwitterIcon.png" alt="twitter-icon"></img></a>
+           <img src="images/MoonFlameGames_512x512_white.png" alt="MoonFlameGames"></img>
           <br>
           <br>
           <a href="https://www.patreon.com/moonflamegames" target="_blank"><img src="images/PatreonLogo.png" alt="patreon-icon"></img></a><br>
--- js/ger_header.js
+++ js/ger_header.js
@@ -11,7 +11,7 @@ document.write(`
           <div class="link"><a href="ger_world.html">Welt</a></div>
           <div class="link"><a href="ger_cast_bachelors.html">Bewohner</a></div>
           <!-- <div class="link"><a href="ger_farm.html">Farm</a></div>-->
-          <div class="link"><a href="ger_blog_23.html">Blog</a></div>
+          <div class="link"><a href="ger_blog_24.html">Blog</a></div>
           <div class="link"><a href="ger_jobs.html">Jobs</a></div>
 
         </ul>
@@ -22,6 +22,7 @@ document.write(`
       <u><a href="press/product/ger_index.html" target="_blank">Presskit</a></u>
           <a href="https://discord.gg/8tXMkrMDyy" target="_blank"> <img src="images/DiscordIcon.png" alt="discord-icon"></img></a>
           <a href="https://twitter.com/MagicalHarvest" target="_blank"><img src="images/TwitterIcon.png" alt="twitter-icon"></img></a>
+          <img src="images/MoonFlameGames_512x512_white.png" alt="MoonFlameGames"></img>
           <br>
           <br>
           <a href="https://www.patreon.com/moonflamegames" target="_blank"><img src="images/PatreonLogo.png" alt="patreon-icon"></img></a><br>
